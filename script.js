ae61c8c4760c3c4969c4d3135609efced2eff6de
huichingheng <huichingheng@gmail.com>
2018-06-13 00:24:20 +0800

    done

---
+++ script.js
@@ -0,0 +1,17 @@
+// function getNumbertoSmaller() {
+//     document.body.className='smaller';
+// }
+
+// function updateClassToNothing() {
+//     document.body.className='';
+// }
+
+// function getNumbertoBigger() {
+//     document.body.className='bigger';
+// }
+
+function number(className) {
+    document.body.className=className;
+}
+
+
